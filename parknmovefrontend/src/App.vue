3f535d75452e12e402144642078a6654665da2d5
Joaquin Pinto <joaquin-pinto@alumnos.ucn.cl>
2023-09-27 10:59:34 -0300

    template v1

--- parknmovefrontend/src/App.vue
+++ parknmovefrontend/src/App.vue
@@ -1,47 +1,86 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<!--
+=========================================================
+* Vue Argon Dashboard 2 - v3.0.0
+=========================================================
 
+* Product Page: https://creative-tim.com/product/vue-argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <div
+    v-show="this.$store.state.layout === 'landing'"
+    class="landing-bg h-100 bg-gradient-primary position-fixed w-100"
+  ></div>
+  <sidenav
+    :custom_class="this.$store.state.mcolor"
+    :class="[
+      this.$store.state.isTransparent,
+      this.$store.state.isRTL ? 'fixed-end' : 'fixed-start'
+    ]"
+    v-if="this.$store.state.showSidenav"
+  />
+  <main
+    class="main-content position-relative max-height-vh-100 h-100 border-radius-lg"
+  >
+    <!-- nav -->
+    <navbar
+      :class="[navClasses]"
+      :textWhite="
+        this.$store.state.isAbsolute ? 'text-white opacity-8' : 'text-white'
+      "
+      :minNav="navbarMinimize"
+      v-if="this.$store.state.showNavbar"
+    />
+    <router-view />
+    <app-footer v-show="this.$store.state.showFooter" />
+    <configurator
+      :toggle="toggleConfigurator"
+      :class="[
+        this.$store.state.showConfig ? 'show' : '',
+        this.$store.state.hideConfigButton ? 'd-none' : ''
+      ]"
+    />
   </main>
 </template>
+<script>
+import Sidenav from "./examples/Sidenav";
+import Configurator from "@/examples/Configurator.vue";
+import Navbar from "@/examples/Navbars/Navbar.vue";
+import AppFooter from "@/examples/Footer.vue";
+import { mapMutations } from "vuex";
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  name: "App",
+  components: {
+    Sidenav,
+    Configurator,
+    Navbar,
+    AppFooter
+  },
+  methods: {
+    ...mapMutations(["toggleConfigurator", "navbarMinimize"])
+  },
+  computed: {
+    navClasses() {
+      return {
+        "position-sticky bg-white left-auto top-2 z-index-sticky":
+          this.$store.state.isNavFixed && !this.$store.state.darkMode,
+        "position-sticky bg-default left-auto top-2 z-index-sticky":
+          this.$store.state.isNavFixed && this.$store.state.darkMode,
+        "position-absolute px-4 mx-0 w-100 z-index-2": this.$store.state
+          .isAbsolute,
+        "px-0 mx-4": !this.$store.state.isAbsolute
+      };
+    }
+  },
+  beforeMount() {
+    this.$store.state.isTransparent = "bg-transparent";
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+};
+</script>
