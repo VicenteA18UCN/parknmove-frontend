d2d699fe39e2aab07e0f6b77efb423221f1557dc
Vicente Alarcón <vicente.alarcon@alumnos.ucn.cl>
2023-12-03 11:15:08 -0300

    feat: added test for empty login form

--- src/app/tests/Login.test.js
+++ src/app/tests/Login.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import toast from "react-toastify";
 import Login from "../../features/auth/Login";
 import { Provider } from "react-redux";
 import { store } from "../../app/store/store";
@@ -7,6 +8,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "../../app/router/Routes";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "../../app/store/store";
+import userEvent from "@testing-library/user-event";
 
 describe("Login", () => {
   test("renders Login component", async () => {
@@ -22,5 +24,46 @@ describe("Login", () => {
 
     const element = await screen.findByText("Inicia Sesión");
     expect(element).toBeInTheDocument();
+  });
+
+  test("renders Login form", () => {
+    render(
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <React.StrictMode>
+            <RouterProvider router={router} />
+          </React.StrictMode>
+        </PersistGate>
+      </Provider>
+    );
+
+    const emailInput = screen.getByLabelText(/correo electrónico/i);
+    const passwordInput = screen.getByLabelText(/contraseña/i);
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  test("shows a toast when form is submitted empty", async () => {
+    render(
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <React.StrictMode>
+            <RouterProvider router={router} />
+          </React.StrictMode>
+        </PersistGate>
+      </Provider>
+    );
+
+    const submitButton = screen.getByRole("button", {
+      name: /iniciar sesión/i,
+    });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const toastElement = screen.getByText(/Debe completar todos los campos/i);
+      expect(toastElement).toBeInTheDocument();
+    });
   });
 });
