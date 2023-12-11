76ac76ebaee7b62171c01a3e15f1c019a92a1dff
VicenteA18UCN <105085389+VicenteA18UCN@users.noreply.github.com>
2023-12-11 02:59:48 -0300

    fix: fix url in agent

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { store } from "../store/store";
 
-axios.defaults.baseURL = "http://localhost:4000/";
+axios.defaults.baseURL = "http://parknmovebackend.azurewebsites.net:4000/";
 
 const responseBody = (response) => response.data;
 axios.defaults.withCredentials = true;
@@ -18,7 +18,7 @@ axios.interceptors.request.use((config) => {
 });
 
 /**
- * Este interceptor se encarga de capturar las solicitudas y capturar las respuestas que se produzcan en las peticiones 
+ * Este interceptor se encarga de capturar las solicitudas y capturar las respuestas que se produzcan en las peticiones
  * al servidor y de mostrarlos en la consola.
  */
 
@@ -38,8 +38,7 @@ const GetUsers = {
 };
 
 const Update = {
-  update: (user) =>
-  requests.put(`user/updateUser/`, { user }),
+  update: (user) => requests.put(`user/updateUser/`, { user }),
 };
 
 const GetParkings = {
@@ -48,18 +47,16 @@ const GetParkings = {
 };
 
 const EditParking = {
-  editParking: (newParking) => requests.put(`parking/editParking`, { newParking }),
+  editParking: (newParking) =>
+    requests.put(`parking/editParking`, { newParking }),
 };
 
 const GetReservations = {
   getReservations: () => requests.get("/reservations/history"),
 };
 
-
 const Search = {
-  searchUser: (
-    searchData
-  ) => requests.post("user/searchUser", { searchData }),
+  searchUser: (searchData) => requests.post("user/searchUser", { searchData }),
 };
 
 const agent = {
@@ -69,7 +66,7 @@ const agent = {
   GetParkings,
   EditParking,
   GetReservations,
-  Search
+  Search,
 };
 
 export default agent;
